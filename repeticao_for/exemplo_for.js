3312a80f8327c62c5874a5d677a3c50c1e3b21af
Erick Fernando Martins Santos <141879391+erickovisck@users.noreply.github.com>
2024-09-28 17:41:38 -0300

    Add files via upload

---
+++ repeticao_for/exemplo_for.js
@@ -0,0 +1,7 @@
+function for_func(){
+    let num = parseInt(document.getElementById('numero').value)
+    for(let i = 1; i<11; i++){
+                    document.write(i, "x", num, "=", i*num, "<br>")
+
+    }
+}
